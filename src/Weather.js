288c5f22adf53b90e774c96d2ce84decf230161e
Sally Lee-Duffy <sally@thesnowinstitute.com>
2023-12-11 18:17:12 +0100

    Added in weather react project from sandbox

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,76 @@
 import React from "react";
-import axios from "axios";
-
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}C`
-    );
-  }
-  let apiKey = "094780c710fa4efd669f0df8c3991927";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+export default function App() {
+  return (
+    <div className="Weather">
+      <div class="weather-app-wrapper">
+        <div class="weather-container">
+          <form id="search-form">
+            <div class="row">
+              <div class="col-9">
+                <input
+                  type="search"
+                  placeholder="Type a city"
+                  class="form-control"
+                  id="form-search-box"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="col-3">
+                <input
+                  type="submit"
+                  value="search"
+                  class="btn w-100"
+                  id="button-search-form"
+                />
+              </div>
+            </div>
+          </form>
+          <div class="overview">
+            <div class="row">
+              <div class="col-6">
+                <h1 id="city">New York</h1>
+              </div>
+              <div class="col-6">
+                <div class="weather-temperature">
+                  <img src="" alt="" id="icon" />
+                  <strong id="temperature">19</strong>
+                  <span class="units">ºC</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <ul>
+                <li id="time"></li>
+                <li id="date"></li>
+              </ul>
+            </div>
+            <div class="col-6">
+              <ul>
+                <li id="description"></li>
+                <li>
+                  Humidity: <span id="humidity">0</span>%
+                </li>
+                <li>
+                  Wind: <span id="wind">0</span> km/h
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="weather-forecast" id="forecast"></div>
+        </div>
+        <small>
+          <a
+            href="https://github.com/sallyelaineleeduffy/weather-app-wk7"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open Source Code
+          </a>
+          by Sally Lee-Duffy
+        </small>
+      </div>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Weather from "./Weather";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./Weather.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Weather />
   </React.StrictMode>
 );
 
